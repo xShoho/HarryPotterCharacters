3b489014a14c6d2fd48fd5a65c5f5f90055537af
Kamil Starosta <ks@g3w.eu>
2024-12-01 16:54:33 +0100

    Added README
            Added README.md
            Removed clearData.py and put its code into main.py

---
+++ README.md
@@ -0,0 +1,18 @@
+# Harry Potter Characters
+
+> Scrapper from HP [wiki](https://heroes-and-villain.fandom.com/wiki/Category:Harry_Potter_characters)
+
+Returns csv file containing name and surname of Harry Potter characters
+
+## Requirements
+- requests
+- pandas
+- BeautifulSoup:
+
+```pip install pandas beautifulsoup4 requests```
+
+## Usage
+
+To use this, simply run 
+
+```python main.py```
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 
@@ -21,10 +20,45 @@ for character in characters:
         "surname": character.text.split(" ")[-1]
     })
 
-for character in all_characters:
-    print(f'Name: { character['name'] }\nSurname: { character['surname'] }\n\n')
+data = pd.DataFrame.from_dict(all_characters, )
 
+data.loc[data['surname'].str.endswith(')', na= False), 'name'] += ' ' + data['surname']
+data.loc[data['surname'].str.endswith(')', na= False), 'surname'] = ''
 
-df = pd.DataFrame.from_dict(all_characters, )
+data.loc[data['name'].isna() | (data['name'] == ''), 'name'] = data['surname']
+data.loc[data['name'] == data['surname'], 'surname'] = ''
+data.loc[data['name'] == 'Lily L.', 'name'] = 'Lily Luna'
+data.loc[data['name'] == 'James S.', 'name'] = 'James Severus'
 
-df.to_csv('./HarryPotterCharacters.csv', index= False)
+data = data[~data['name'].str.endswith(')', na= False)]
+data = data[~data['name'].str.startswith('Category', na= False)]
+data = data[~data['name'].str.startswith('British', na= False)]
+data = data[~data['name'].str.startswith('Department', na= False)]
+data = data[~data['surname'].str.startswith('Army', na= False)]
+
+data = data[~data['name'].str.startswith('Gryffindor', na= False)]
+data = data[~data['name'].str.startswith('Hogwarts', na= False)]
+data = data[~data['name'].str.startswith('Hufflepuff', na= False)]
+data = data[~data['name'].str.startswith('Ilvermorny', na= False)]
+data = data[~data['name'].str.startswith('List', na= False)]
+data = data[~data['name'].str.startswith('Order', na= False)]
+data = data[~data['name'].str.startswith('Ravenclaw', na= False)]
+data = data[~data['name'].str.startswith('Serpent', na= False)]
+data = data[~data['name'].str.startswith('Slytherin', na= False)]
+data = data[~data['name'].str.startswith('House', na= False)]
+
+data = data[~data['surname'].str.startswith('Weasleys', na= False)]
+data = data[~data['surname'].str.startswith('Potters', na= False)]
+
+data = data[~data['name'].str.startswith('Mr. and Mrs.', na= False)]
+
+grangers = pd.DataFrame({
+    'name': ['Mr. Granger', 'Mrs. Granger'],
+    'surname': [None, None]
+})
+
+data = pd.concat([data, grangers], ignore_index= True)
+
+data = data.rename(columns= {'name': 'character_name', 'surname': 'character_surname'})
+
+data.to_csv('./HarryPotterCharacters.csv', index= False)
